11bbf96f2ec987684e06336999724b6e1b0a775c
timbundon <timburhas2011@gmail.com>
2026-04-17 13:57:04 +0500

    asdas

---
+++ core/common.py
@@ -0,0 +1,47 @@
+import ctypes
+import logging
+import base64
+import os
+
+class Config:
+    def __init__(self):
+        self.PORT = 5000
+        self.TOKEN = base64.b64decode("cHIwZ3Jh").decode()
+
+class EventBus:
+    def __init__(self):
+        self.listeners = {}
+    def subscribe(self, event_name, callback):
+        if event_name not in self.listeners:
+            self.listeners[event_name] = []
+        self.listeners[event_name].append(callback)
+    def emit(self, event_name, *args, **kwargs):
+        if event_name in self.listeners:
+            for callback in self.listeners[event_name]:
+                callback(*args, **kwargs)
+
+def configure_logging(log_file):
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s [%(levelname)s] %(message)s',
+        handlers=[
+            logging.FileHandler(log_file),
+            logging.StreamHandler()
+        ]
+    )
+
+def toogle_console(visiblity):
+    SW_HIDE = 0
+    SW_SHOW = 5
+    GWL_EXSTYLE = -20
+    WS_EX_TOOLWINDOW = 0x00000080
+    WS_EX_APPWINDOW = 0x00040000
+    hwnd = ctypes.windll.kernel32.GetConsoleWindow()
+    if hwnd:
+        ctypes.windll.user32.ShowWindow(hwnd, SW_SHOW if visiblity else SW_HIDE)
+        style = ctypes.windll.user32.GetWindowLongW(hwnd, GWL_EXSTYLE)
+        style &= ~WS_EX_TOOLWINDOW
+        style |= WS_EX_APPWINDOW
+        ctypes.windll.user32.SetWindowLongW(hwnd, GWL_EXSTYLE, style)
+
+BUS = EventBus()
